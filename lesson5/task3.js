654f2a90b9614187542e4420fa682ba132a071ca
Alexander2951 <111084261+Alexander2951@users.noreply.github.com>
2023-05-01 21:23:09 +0300

    Add files via upload

---
+++ lesson5/task3.js
@@ -0,0 +1,21 @@
+// Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
+
+const products = [
+{
+id: 3,
+price: 200,
+},
+{
+id: 4,
+price: 900,
+},
+{
+id: 1,
+price: 1000,
+},
+];
+products.forEach (function (elem) {
+    elem.price = elem.price * 0.85;
+});
+console.log (products);
+// Ух ты сработало ! Но как я не понял...
